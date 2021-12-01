bf6a89ee852438bf04c92efcb5ef6470bf17b7a7
Davi Schilling <davischilling.dev@gmail.com>
2021-12-01 18:59:14 +0100

    chore: project implementation

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "./index.css";
+
+import App from "./Containers/App/App";
+// import AppHooks from './Containers/App/AppHooks';
+// import AppReduxConnect from './Containers/App/AppRedux.connect';
+
+import "tachyons"; // Templates
+
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import { searchRobots, requestRobots } from './Containers';
+
+// middlewares
+// import { createLogger } from 'redux-logger';
+// import thunkMiddleware from 'redux-thunk';
+
+// const logger = createLogger();
+// const rootReducer = combineReducers({ searchRobots, requestRobots })
+// const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />,{/* <AppHooks />, */}
+    {/* <Provider store={store}>
+      <AppReduxConnect />
+    </Provider> */}
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
